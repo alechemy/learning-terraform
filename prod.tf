76010ac21b12a5e51c5e3378b7eeeaaf43188968
Alec Custer <alcuster@icloud.com>
2022-08-25 13:57:54 -0700

    define initial web security group

--- prod.tf
+++ prod.tf
@@ -13,3 +13,37 @@ resource "aws_s3_bucket_acl" "prod_tf_course_bucket_acl" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports inbound and everything outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    # any IP
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    # all open
+    from_port = 0
+    to_port   = 0
+    # all protocols out
+    protocol = "-1"
+    # any IP
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
