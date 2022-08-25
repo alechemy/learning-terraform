d89c147ea58044a54c9e7ea7c8bfa109bd445ffa
Alec Custer <alcuster@icloud.com>
2022-08-25 15:06:21 -0700

    refactor to use asg

--- prod.tf
+++ prod.tf
@@ -62,38 +62,8 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami           = "ami-0bf2de022ba73d0be"
-  instance_type = "t2.nano"
-
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-# De-couple creation of IP and its assignment
-resource "aws_eip_association" "prod_web" {
-  instance_id   = aws_instance.prod_web[0].id
-  allocation_id = aws_eip.prod_web.id
-}
-
-# static IP
-resource "aws_eip" "prod_web" {
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
 resource "aws_elb" "prod_web" {
-  name = "prod-web"
-  # all instances
-  instances       = aws_instance.prod_web[*].id
+  name            = "prod-web"
   subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id]
 
@@ -107,4 +77,37 @@ resource "aws_elb" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
+}
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-0bf2de022ba73d0be"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
 }
